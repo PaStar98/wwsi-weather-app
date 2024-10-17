cf69fbcb0c77070a7ef7c56bc4bfcad6ce0d6430
PaStar98 <startekp98@gmail.com>
2024-10-17 21:22:18 +0200

    feat: loading spinner

--- src/routes/CityWeatherDetailsPage.tsx
+++ src/routes/CityWeatherDetailsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import React from 'react'
 import { Box, Paper, Typography } from '@mui/material'
@@ -9,18 +9,22 @@ import WeatherResponse from '../api/WeatherService/WeatherResponse'
 import PixabayResponse from '../api/PixabayService/PixabayResponse'
 import getRandomNumberInRange from '../utils/getRandomNumberInRange'
 import joinWordsWithPlus from '../utils/joinWordsWithPlus'
+import LoadingSpinner from '../components/LoadingSpinner'
 
 const CityWeatherDetailsPage: React.FC = () => {
   const { city } = useParams<{ city: string }>()
-  const [currentWeather, setCurrentWeather] = React.useState<WeatherResponse>()
-  const [currentWeatherImage, setCurrentWeatherImage] = React.useState<PixabayResponse>()
+  const [currentWeather, setCurrentWeather] = useState<WeatherResponse>()
+  const [currentWeatherImage, setCurrentWeatherImage] = useState<PixabayResponse>()
+  const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
     const fetchCurrentWeather = async () => {
+      setLoading(true)
       const weatherService = new WeatherApiService()
       const weatherData: WeatherResponse = await weatherService.getWeatherByCity(city?.toString())
       setCurrentWeather(weatherData)
       console.log(weatherData)
+      setLoading(false)
     }
 
     fetchCurrentWeather()
@@ -28,16 +32,22 @@ const CityWeatherDetailsPage: React.FC = () => {
 
   useEffect(() => {
     const fetchWeatherImage = async () => {
+      setLoading(true)
       const weatherDescription = joinWordsWithPlus(currentWeather?.weather[0].description)
       const pixabayService = new PixabayApiService()
       const pixabayData: PixabayResponse =
         await pixabayService.getImageByCityWeather(weatherDescription)
       setCurrentWeatherImage(pixabayData)
       console.log(pixabayData)
+      setLoading(false)
     }
 
     fetchWeatherImage()
   }, [currentWeather])
+
+  if (loading) {
+    return <LoadingSpinner />
+  }
 
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', p: 2 }}>
